d93a1b307760c8a1776f0f6af2330322286d24f5
Zachary Chua <zacchua@gmail.com>
2022-01-08 02:31:54 +0800

    Added login stuff

--- frontend/src/Auth.jsx
+++ frontend/src/Auth.jsx
@@ -1,13 +1,21 @@
 import "./style/Auth.css";
 import "./style/Button.css";
+import { supabase } from "./supabaseClient";
 
-function Auth() {
+function Auth(props) {
+  async function signInWithGoogle() {
+    const { user, session, error } = await supabase.auth.signIn({
+      provider: "google",
+    });
+  }
+
   return (
     <div class="auth">
       <h1 class="text">Login</h1>
       <button
         class="button"
         style={{ "background-color": "#DB4437", color: "white" }}
+        onClick={signInWithGoogle}
       >
         Google
       </button>
