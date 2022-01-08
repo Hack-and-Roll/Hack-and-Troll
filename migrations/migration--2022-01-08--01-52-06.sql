7ec72bdae8d2f6c69a757aedce146cbeec07ff94
Benjamin Tan <benjamin@dev.ofcr.se>
2022-01-08 11:00:10 +0800

    Add component values

--- migrations/migration--2022-01-08--01-52-06.sql
+++ migrations/migration--2022-01-08--01-52-06.sql
@@ -2,46 +2,179 @@
 insert into public.components (type, name, rarity)
   select
     'background' as type,
-    'Background ' || i as name,
-    0.1 as rarity
-  from generate_series(1, 100) as i;
+    'plain' as name,
+    0.6 as rarity
+  from generate_series(1, 3000);
+  
+insert into public.components (type, name, rarity)
+  select
+    'background' as type,
+    'cloud' as name,
+    0.2 as rarity
+  from generate_series(1, 1000);
+
+insert into public.components (type, name, rarity)
+  select
+    'background' as type,
+    'forest' as name,
+    0.15 as rarity
+  from generate_series(1, 750);
+
+insert into public.components (type, name, rarity)
+  select
+    'background' as type,
+    'space' as name,
+    0.05 as rarity
+  from generate_series(1, 250);
 
 -- Face
 insert into public.components (type, name, rarity)
   select
     'face' as type,
-    'Face ' || i as name,
+    'dude' as name,
+    0.05 as rarity
+  from generate_series(1, 250);
+
+insert into public.components (type, name, rarity)
+  select
+    'face' as type,
+    'tude' as name,
+    0.15 as rarity
+  from generate_series(1, 750);
+
+insert into public.components (type, name, rarity)
+  select
+    'face' as type,
+    'aude' as name,
+    0.2 as rarity
+  from generate_series(1, 1000);
+
+insert into public.components (type, name, rarity)
+  select
+    'face' as type,
+    'bude' as name,
+    0.5 as rarity
+  from generate_series(1, 2500);
+
+insert into public.components (type, name, rarity)
+  select
+    'face' as type,
+    'cude' as name,
     0.1 as rarity
-  from generate_series(1, 100) as i;
+  from generate_series(1, 500);
 
 -- Hat
 insert into public.components (type, name, rarity)
   select
     'hat' as type,
-    'Hat ' || i as name,
+    'halo' as name,
+    0.05 as rarity
+  from generate_series(1, 250);
+
+insert into public.components (type, name, rarity)
+  select
+    'hat' as type,
+    'cap' as name,
+    0.15 as rarity
+  from generate_series(1, 750);
+
+insert into public.components (type, name, rarity)
+  select
+    'hat' as type,
+    'tap' as name,
+    0.2 as rarity
+  from generate_series(1, 1000);
+
+insert into public.components (type, name, rarity)
+  select
+    'hat' as type,
+    'pap' as name,
+    0.5 as rarity
+  from generate_series(1, 2500);
+
+insert into public.components (type, name, rarity)
+  select
+    'hat' as type,
+    'jap' as name,
     0.1 as rarity
-  from generate_series(1, 100) as i;
+  from generate_series(1, 500);
 
 -- Hands
 insert into public.components (type, name, rarity)
   select
     'hands' as type,
-    'Hands ' || i as name,
-    0.1 as rarity
-  from generate_series(1, 100) as i;
+    'normal' as name,
+    0.5 as rarity
+  from generate_series(1, 2500);
+
+insert into public.components (type, name, rarity)
+  select
+    'hands' as type,
+    'robot' as name,
+    0.3 as rarity
+  from generate_series(1, 1500);
+
+insert into public.components (type, name, rarity)
+  select
+    'hands' as type,
+    'armless' as name,
+    0.2 as rarity
+  from generate_series(1, 1000);
 
 -- Body
 insert into public.components (type, name, rarity)
   select
     'body' as type,
-    'Body ' || i as name,
-    0.1 as rarity
-  from generate_series(1, 100) as i;
+    'normal' as name,
+    0.7 as rarity
+  from generate_series(1, 3500);
+
+insert into public.components (type, name, rarity)
+  select
+    'body' as type,
+    'beach' as name,
+    0.3 as rarity
+  from generate_series(1, 1500);
 
 -- Pet
 insert into public.components (type, name, rarity)
   select
     'pet' as type,
-    'Pet ' || i as name,
-    0.1 as rarity
-  from generate_series(1, 100) as i;
+    'bat' as name,
+    0.4 as rarity
+  from generate_series(1, 2000);
+
+insert into public.components (type, name, rarity)
+  select
+    'pet' as type,
+    'dog' as name,
+    0.2 as rarity
+  from generate_series(1, 1000);
+
+insert into public.components (type, name, rarity)
+  select
+    'pet' as type,
+    'yat' as name,
+    0.15 as rarity
+  from generate_series(1, 750);
+
+insert into public.components (type, name, rarity)
+  select
+    'pet' as type,
+    'dat' as name,
+    0.15 as rarity
+  from generate_series(1, 750);
+
+insert into public.components (type, name, rarity)
+  select
+    'pet' as type,
+    'aat' as name,
+    0.05 as rarity
+  from generate_series(1, 250);
+
+insert into public.components (type, name, rarity)
+  select
+    'pet' as type,
+    'pat' as name,
+    0.05 as rarity
+  from generate_series(1, 250);
---
+++ migrations/migration--2022-01-08--10-58-50.sql
@@ -0,0 +1,1 @@
+alter table public.users alter column money set default 200;
